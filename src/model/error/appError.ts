005cc5dc9b4eec8195d04df40ee7c9746fcc0c09
João Victor <211412399+VictorM-Dev@users.noreply.github.com>
2026-04-14 17:03:43 -0300

    feat: implements appError

---
+++ src/model/error/appError.ts
@@ -0,0 +1,11 @@
+class AppError extends Error{
+  constructor(message: string, public status: number, public type: string){
+    super(message);
+  }
+}
+
+export class ArgumentRequired extends AppError{
+  constructor(message: string){
+    super(message, 400, "bad-request");
+  }
+}
